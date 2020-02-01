800120f0c76d88a9f660e809ecb3b2c9c280bd71
Gabriel <gfursten@amazon.com>
2020-02-01 14:18:26 +0100

    Fix failing test cases

--- prepare-commit-msg-hook.sh
+++ prepare-commit-msg-hook.sh
@@ -5,6 +5,22 @@ if [ -z "$BRANCH_NAME" ]; then
     exit 1
 fi
 
+if [[ ! "$BRANCH_NAME" =~ ^[A-Z]+-[0-9]+ ]]; then
+  ## Branch is not following team convention. This could happen for example in master. Nothing to be done
+  exit 0
+fi
+
+COMMIT=$(cat $1)
+if [[ "$COMMIT" =~ ^\[[A-Z]+-[0-9]+\] ]]; then
+  ## There is already some code in the commit. For example from ammending or applying. Nothing to be done.
+  exit 0
+fi
+
+if [[ "$COMMIT" =~ ^Merge[[:space:]]branch[[:space:]] ]]; then
+  ## Let's not modify automated messages
+ exit 0
+fi
+
 ## Get code from branch
 CODE=$(echo $BRANCH_NAME | sed -E "s/^([A-Z]+-[0-9]+).*/\1/")
 sed -i -e "1s/^/[$CODE] /" $1
